0a654f8531eb3e76fc562e97d3bc930f7bc9ea6f
zici <1940699+zizicici@users.noreply.github.com>
2023-07-08 22:45:21 +0800

    [init] Initial project

---
+++ moontake/UIColor+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+Extension.swift
+//  moontake
+//
+//  Created by Ci Zi on 2023/7/8.
+//
+
+
+import UIKit
+
+extension UIColor {
+    static let backgroundColor: UIColor = UIColor(named: "BackgroundColor") ?? UIColor.systemBackground
+}
