021d0ae7abc6810c83edd4d52cfe64681c981e7d
zici <1940699+zizicici@users.noreply.github.com>
2023-07-27 00:27:27 +0800

    [misc] Remove useless code

--- moontake/PhotoCaptureDelegate.swift
+++ moontake/PhotoCaptureDelegate.swift
@@ -13,7 +13,6 @@ class PhotoCaptureProcessor: NSObject {
     
     private let willCapturePhotoAnimation: () -> Void
     
-    
     lazy var context = CIContext()
     
     private let completionHandler: (PhotoCaptureProcessor) -> Void
@@ -22,11 +21,6 @@ class PhotoCaptureProcessor: NSObject {
     
     private var photoData: Data?
     
-    private var livePhotoCompanionMovieURL: URL?
-    
-    private var portraitEffectsMatteData: Data?
-    
-    private var semanticSegmentationMatteDataArray = [Data]()
     private var maxPhotoProcessingTime: CMTime?
 
     // Save the location of captured photos
@@ -43,16 +37,6 @@ class PhotoCaptureProcessor: NSObject {
     }
     
     private func didFinish() {
-        if let livePhotoCompanionMoviePath = livePhotoCompanionMovieURL?.path {
-            if FileManager.default.fileExists(atPath: livePhotoCompanionMoviePath) {
-                do {
-                    try FileManager.default.removeItem(atPath: livePhotoCompanionMoviePath)
-                } catch {
-                    print("Could not remove file at url: \(livePhotoCompanionMoviePath)")
-                }
-            }
-        }
-        
         completionHandler(self)
     }
 }
@@ -82,52 +66,6 @@ extension PhotoCaptureProcessor: AVCapturePhotoCaptureDelegate {
         }
     }
     
-    func handleMatteData(_ photo: AVCapturePhoto, ssmType: AVSemanticSegmentationMatte.MatteType) {
-        
-        // Find the semantic segmentation matte image for the specified type.
-        guard var segmentationMatte = photo.semanticSegmentationMatte(for: ssmType) else { return }
-        
-        // Retrieve the photo orientation and apply it to the matte image.
-        if let orientation = photo.metadata[String(kCGImagePropertyOrientation)] as? UInt32,
-            let exifOrientation = CGImagePropertyOrientation(rawValue: orientation) {
-            // Apply the Exif orientation to the matte image.
-            segmentationMatte = segmentationMatte.applyingExifOrientation(exifOrientation)
-        }
-        
-        var imageOption: CIImageOption!
-        
-        // Switch on the AVSemanticSegmentationMatteType value.
-        switch ssmType {
-        case .hair:
-            imageOption = .auxiliarySemanticSegmentationHairMatte
-        case .skin:
-            imageOption = .auxiliarySemanticSegmentationSkinMatte
-        case .teeth:
-            imageOption = .auxiliarySemanticSegmentationTeethMatte
-        case .glasses:
-            imageOption = .auxiliarySemanticSegmentationGlassesMatte
-        default:
-            print("This semantic segmentation type is not supported!")
-            return
-        }
-        
-        guard let perceptualColorSpace = CGColorSpace(name: CGColorSpace.sRGB) else { return }
-        
-        // Create a new CIImage from the matte's underlying CVPixelBuffer.
-        let ciImage = CIImage( cvImageBuffer: segmentationMatte.mattingImage,
-                               options: [imageOption: true,
-                                         .colorSpace: perceptualColorSpace])
-        
-        // Get the HEIF representation of this image.
-        guard let imageData = context.heifRepresentation(of: ciImage,
-                                                         format: .RGBA8,
-                                                         colorSpace: perceptualColorSpace,
-                                                         options: [.depthImage: ciImage]) else { return }
-        
-        // Add the image data to the SSM data array for writing to the photo library.
-        semanticSegmentationMatteDataArray.append(imageData)
-    }
-    
     /// - Tag: DidFinishProcessingPhoto
     func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
         photoProcessingHandler(false)
@@ -138,42 +76,10 @@ extension PhotoCaptureProcessor: AVCapturePhotoCaptureDelegate {
         } else {
             photoData = photo.fileDataRepresentation()
         }
-        // A portrait effects matte gets generated only if AVFoundation detects a face.
-        if var portraitEffectsMatte = photo.portraitEffectsMatte {
-            if let orientation = photo.metadata[ String(kCGImagePropertyOrientation) ] as? UInt32 {
-                portraitEffectsMatte = portraitEffectsMatte.applyingExifOrientation(CGImagePropertyOrientation(rawValue: orientation)!)
-            }
-            let portraitEffectsMattePixelBuffer = portraitEffectsMatte.mattingImage
-            let portraitEffectsMatteImage = CIImage( cvImageBuffer: portraitEffectsMattePixelBuffer, options: [ .auxiliaryPortraitEffectsMatte: true ] )
-            
-            guard let perceptualColorSpace = CGColorSpace(name: CGColorSpace.sRGB) else {
-                portraitEffectsMatteData = nil
-                return
-            }
-            portraitEffectsMatteData = context.heifRepresentation(of: portraitEffectsMatteImage,
-                                                                  format: .RGBA8,
-                                                                  colorSpace: perceptualColorSpace,
-                                                                  options: [.portraitEffectsMatteImage: portraitEffectsMatteImage])
-        } else {
-            portraitEffectsMatteData = nil
-        }
-        
-        for semanticSegmentationType in output.enabledSemanticSegmentationMatteTypes {
-            handleMatteData(photo, ssmType: semanticSegmentationType)
-        }
     }
     
     /// - Tag: DidFinishRecordingLive
     func photoOutput(_ output: AVCapturePhotoOutput, didFinishRecordingLivePhotoMovieForEventualFileAt outputFileURL: URL, resolvedSettings: AVCaptureResolvedPhotoSettings) {
-    }
-    
-    /// - Tag: DidFinishProcessingLive
-    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingLivePhotoToMovieFileAt outputFileURL: URL, duration: CMTime, photoDisplayTime: CMTime, resolvedSettings: AVCaptureResolvedPhotoSettings, error: Error?) {
-        if error != nil {
-            print("Error processing Live Photo companion movie: \(String(describing: error))")
-            return
-        }
-        livePhotoCompanionMovieURL = outputFileURL
     }
     
     /// - Tag: DidFinishCapture
@@ -200,30 +106,6 @@ extension PhotoCaptureProcessor: AVCapturePhotoCaptureDelegate {
                     
                     // Specify the location the photo was taken
                     creationRequest.location = self.location
-                    
-                    if let livePhotoCompanionMovieURL = self.livePhotoCompanionMovieURL {
-                        let livePhotoCompanionMovieFileOptions = PHAssetResourceCreationOptions()
-                        livePhotoCompanionMovieFileOptions.shouldMoveFile = true
-                        creationRequest.addResource(with: .pairedVideo,
-                                                    fileURL: livePhotoCompanionMovieURL,
-                                                    options: livePhotoCompanionMovieFileOptions)
-                    }
-                    
-                    // Save Portrait Effects Matte to Photos Library only if it was generated
-                    if let portraitEffectsMatteData = self.portraitEffectsMatteData {
-                        let creationRequest = PHAssetCreationRequest.forAsset()
-                        creationRequest.addResource(with: .photo,
-                                                    data: portraitEffectsMatteData,
-                                                    options: nil)
-                    }
-                    // Save Portrait Effects Matte to Photos Library only if it was generated
-                    for semanticSegmentationMatteData in self.semanticSegmentationMatteDataArray {
-                        let creationRequest = PHAssetCreationRequest.forAsset()
-                        creationRequest.addResource(with: .photo,
-                                                    data: semanticSegmentationMatteData,
-                                                    options: nil)
-                    }
-                    
                 }, completionHandler: { _, error in
                     if let error = error {
                         print("Error occurred while saving photo to photo library: \(error)")
