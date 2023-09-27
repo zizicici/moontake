f676916f3d40409212c6c4d10faad6ede924848b
zici <1940699+zizicici@users.noreply.github.com>
2023-09-27 22:24:33 +0800

    [misc] Bump version to 1.1.2

--- moontake/More/Controller/SpecificationsViewController.swift
+++ moontake/More/Controller/SpecificationsViewController.swift
@@ -70,7 +70,7 @@ class SpecificationsViewController: UIViewController {
                 case .publisher:
                     return "ZIZICICI LIMITED"
                 case .date:
-                    return "2023/09/05"
+                    return "2023/09/27"
                 }
             }
         }
