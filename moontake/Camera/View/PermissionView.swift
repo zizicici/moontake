ab543c69aad12c5e1cc1e14f681cbd32d245822d
zizicici <1940699+zizicici@users.noreply.github.com>
2024-07-12 11:50:50 +0800

    [feat] Update PermissionView UI

--- moontake/Camera/View/PermissionView.swift
+++ moontake/Camera/View/PermissionView.swift
@@ -11,7 +11,27 @@ import SnapKit
 class PermissionView: UIView {
     private let cameraButton: UIButton = {
         var configuration = UIButton.Configuration.filled()
+        configuration.image = UIImage(systemName: "camera")?.withTintColor(.moonColor, renderingMode: .alwaysOriginal)
+        configuration.imagePadding = 12.0
         configuration.title = String(localized: "Enable Camera Permission")
+        configuration.titleTextAttributesTransformer = UIConfigurationTextAttributesTransformer({ incoming in
+            var outgoing = incoming
+            outgoing.font = UIFont.systemFont(ofSize: 16)
+            outgoing.foregroundColor = UIColor.moonColor
+
+            return outgoing
+        })
+        configuration.subtitle = String(localized: "permission.camera.subtitle")
+        configuration.subtitleTextAttributesTransformer = UIConfigurationTextAttributesTransformer({ incoming in
+            var outgoing = incoming
+            outgoing.font = UIFont.systemFont(ofSize: 12)
+            outgoing.foregroundColor = UIColor.moonColor.withAlphaComponent(0.9)
+
+            return outgoing
+        })
+        configuration.cornerStyle = .large
+        configuration.titlePadding = 10.0
+        configuration.contentInsets = .init(top: 14, leading: 18, bottom: 14, trailing: 18)
         
         let button = UIButton(configuration: configuration)
         button.tintColor = .skyColor
@@ -21,7 +41,27 @@ class PermissionView: UIView {
     
     private let albumButton: UIButton = {
         var configuration = UIButton.Configuration.filled()
+        configuration.image = UIImage(systemName: "photo.on.rectangle")?.withTintColor(.moonColor, renderingMode: .alwaysOriginal)
+        configuration.imagePadding = 12.0
         configuration.title = String(localized: "Enable Add Photo Permission")
+        configuration.titleTextAttributesTransformer = UIConfigurationTextAttributesTransformer({ incoming in
+            var outgoing = incoming
+            outgoing.font = UIFont.systemFont(ofSize: 16)
+            outgoing.foregroundColor = UIColor.moonColor
+
+            return outgoing
+        })
+        configuration.subtitle = String(localized: "permission.addPhoto.subtitle")
+        configuration.subtitleTextAttributesTransformer = UIConfigurationTextAttributesTransformer({ incoming in
+            var outgoing = incoming
+            outgoing.font = UIFont.systemFont(ofSize: 12)
+            outgoing.foregroundColor = UIColor.moonColor.withAlphaComponent(0.9)
+
+            return outgoing
+        })
+        configuration.cornerStyle = .large
+        configuration.titlePadding = 10.0
+        configuration.contentInsets = .init(top: 14, leading: 18, bottom: 14, trailing: 18)
         
         let button = UIButton(configuration: configuration)
         button.tintColor = .skyColor
@@ -33,6 +73,7 @@ class PermissionView: UIView {
         var stackView = UIStackView()
         stackView.axis = .vertical
         stackView.spacing = 40.0
+        stackView.alignment = .center
         
         return stackView
     }()
